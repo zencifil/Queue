9ee7547903ecfcca34cb6b8da1b5347b14689758
Savaş Cilve <savascilve@outlook.com>
2017-12-19 14:54:35 +0000

    Consumer completed for RabbitMQ

--- Queue.RabbitMQ.Client/Program.cs
+++ Queue.RabbitMQ.Client/Program.cs
@@ -1,10 +1,7 @@
 ﻿using RabbitMQ.Client;
 using Newtonsoft.Json;
 using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
 
 namespace Queue.RabbitMQ.Client {
     class Program {
---
+++ Queue.RabbitMQ.Consumer/Program.cs
@@ -0,0 +1,35 @@
+﻿using Newtonsoft.Json;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System;
+using System.Text;
+
+namespace Queue.RabbitMQ.Consumer {
+    class Program {
+        static void Main(string[] args) {
+            ConsumeQueue();
+        }
+
+        private static void ConsumeQueue() {
+            var factory = new ConnectionFactory { HostName = "localhost" };
+            using (IConnection connection = factory.CreateConnection()) {
+                using (IModel channel = connection.CreateModel()) {
+                    channel.QueueDeclare(queue: "Queue.RabbitMQ", durable: false, exclusive: false, autoDelete: false, arguments: null);
+                    var consumer = new EventingBasicConsumer(channel);
+                    consumer.Received += (sender, e) => {
+                        var body = e.Body;
+                        var messageJson = Encoding.UTF8.GetString(body);
+                        var obj = new { name = "", message = "" };
+                        var message = JsonConvert.DeserializeAnonymousType(messageJson, obj);
+                        Console.WriteLine($"Sender: {obj.name}");
+                        Console.WriteLine($"Message: {obj.message}");
+                    };
+                    channel.BasicConsume(queue: "Queue.RabbitMQ", autoAck: true, consumer: consumer);
+
+                    Console.ReadLine();
+                }
+            }
+        }
+        
+    }
+}
